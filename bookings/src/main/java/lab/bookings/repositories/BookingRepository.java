85ccde2a1a5b78592184a55d4a89fdd0c435da38
danielPaszek <daniel.paszek@student.put.poznan.pl>
2024-04-14 14:55:33 +0200

    Lab 5 - Spring

---
+++ bookings/src/main/java/lab/bookings/repositories/BookingRepository.java
@@ -0,0 +1,15 @@
+package lab.bookings.repositories;
+
+import lab.bookings.models.Apartment;
+import lab.bookings.models.Booking;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface BookingRepository extends CrudRepository<Booking, Long> {
+
+
+}
